9060353ab681d8610bc6525ef8d996590917f3e3
MxZX <96307550+MxZX67@users.noreply.github.com>
2026-09-24 22:11:16 +0200

    feat: build LietY2 2.0 run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 set -eu
 make
-if [ ! -f LietY2-disk.img ]; then truncate -s 64M LietY2-disk.img; fi
-qemu-system-x86_64 -m 256M -cdrom LietY2-x86_64.iso -drive file=LietY2-disk.img,format=raw,if=ide -serial stdio
+if [ ! -f LietY2-disk.img ]; then
+    truncate -s 64M LietY2-disk.img
+fi
+qemu-system-x86_64 \
+    -M q35 -m 256M \
+    -cdrom LietY2-x86_64.iso \
+    -drive file=LietY2-disk.img,if=none,id=disk,format=raw \
+    -device ich9-ahci,id=sata0 \
+    -device ide-hd,bus=sata0.0,drive=disk \
+    -boot d -serial stdio
